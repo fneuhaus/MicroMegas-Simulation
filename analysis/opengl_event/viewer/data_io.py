ab8d4fa7d790f4a708ebfbc3e9b79d1093ccebff
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2017-01-18 10:23:55 +0100

    Limit reading tree to needed events.

--- analysis/opengl_event/viewer/data_io.py
+++ analysis/opengl_event/viewer/data_io.py
@@ -6,7 +6,7 @@ from root_numpy import tree2array
 def read_data(input_file_path, tree_name, event=1):
 	input_file = ROOT.TFile(input_file_path, 'read')
 	input_tree = input_file.Get(tree_name)
-	data = tree2array(input_tree)
+	data = tree2array(input_tree, start=event - 1, stop=event, step=1)
 	input_file.Close()
 	event_data = data[event-1] # choose event to display
 	return event_data
