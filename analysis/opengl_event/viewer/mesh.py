130b1b5815c22b813183786b39c8a0a7460c0d52
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2017-04-04 14:28:53 +0200

    Add option to move the z-position of the mesh.

--- analysis/opengl_event/viewer/mesh.py
+++ analysis/opengl_event/viewer/mesh.py
@@ -3,10 +3,13 @@ from pyglet.gl import *
 
 
 class Mesh():
-   def __init__(self, input_file, scale_factor):
+   def __init__(self, input_file, scale_factor, mesh_position):
       self.show = True
+      self.mesh_position = mesh_position
       self.mesh_obj = WavefrontOBJ(input_file, scale_factor=scale_factor)
 
    def draw(self):
       if self.show:
+         glTranslatef(0, 0, self.mesh_position * 1e-3)
          glCallList(self.mesh_obj.gl_list)
+         glTranslatef(0, 0, -self.mesh_position * 1e-3)
