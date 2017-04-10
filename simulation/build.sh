e9a31a6910380c38b275a87b6661aecfd44a9ac7
Friedemann Neuhaus <fneuhaus@students.uni-mainz.de>
2017-04-10 14:15:30 +0200

    Include particleconversion into run manager.
    The particleconversion is now fully included into the run manager. To
    make this possible a few paths had to be adapted, so that e.g. the
    macros could be copied and loaded from the new location easily.

--- simulation/build.sh
+++ simulation/build.sh
@@ -4,15 +4,12 @@ project_path=$(dirname $0)
 project_path=$(readlink -e $project_path)
 export PYTHONPATH="${PYTHONPATH}:$project_path"
 
-if [[ "$1" == "particleconversion" ]]; then
-   echo "$1"
-   echo "Building particleconversion..."
-   cd "$project_path/particleconversion"
-   mkdir -p "build" && cd "build"
-   cmake -DCMAKE_INSTALL_PREFIX=.. ..
-   make recog
-   make
-fi
+echo "Building particleconversion..."
+cd "$project_path/particleconversion"
+mkdir -p "build" && cd "build"
+cmake -DCMAKE_INSTALL_PREFIX=.. ..
+make recog
+make
 
 echo "Building drift..."
 cd "$project_path/drift"
