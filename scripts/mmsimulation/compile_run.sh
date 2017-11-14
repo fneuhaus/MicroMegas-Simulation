6ba399082e9c1d1f771963273102b165e9156696
Friedemann Neuhaus <fneuhaus@students.uni-mainz.de>
2017-11-14 12:22:56 +0100

    Adapt runmanager for COMSOL runs.

--- scripts/mmsimulation/compile_run.sh
+++ scripts/mmsimulation/compile_run.sh
@@ -15,26 +15,21 @@ cleanup() {
 
    # Restore default input files
    mv simulation.conf.orig simulation.conf
-   mv avalanche/geometry/geometry.step.orig avalanche/geometry/geometry.step
 
-   # Find the files to use
-   if [[ "$SIMULATION_CONF" == "" ]] && [[ -f $TARGET_DIR/simulation.conf ]]; then
-      SIMULATION_CONF=$TARGET_DIR/simulation.conf
-   fi
-   if [[ "$MESH_FILE" == "" ]] && [[ -f $TARGET_DIR/geometry/geometry.step ]]; then
-      MESH_FILE=$TARGET_DIR/geometry/geometry.step
-   fi
 }
 
 # Register cleanup
 trap cleanup EXIT ERR INT TERM
 
+# Find the files to use
+if [[ "$SIMULATION_CONF" == "" ]] && [[ -f $TARGET_DIR/simulation.conf ]]; then
+   SIMULATION_CONF=$TARGET_DIR/simulation.conf
+fi
+
 # Backup default files and copy input files to correct location
 cd $MICROMEGAS_SIMULATION_PATH
 cp simulation.conf simulation.conf.orig
-cp avalanche/geometry/geometry.step avalanche/geometry/geometry.step.orig
 [[ -f "$SIMULATION_CONF" ]] && cp "$SIMULATION_CONF" simulation.conf || true
-[[ -f "$MESH_FILE" ]] && cp "$MESH_FILE" avalanche/geometry/geometry.step || true
 
 # Compile simulation
 $MICROMEGAS_SIMULATION_PATH/build.sh > /tmp/micromegas_make.log 2>&1 || exit 1
