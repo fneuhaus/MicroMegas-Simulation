70217f488640198421ff2d034a69d566c8baa8bf
Friedemann Neuhaus <fneuhaus@students.uni-mainz.de>
2017-12-28 13:20:52 +0100

    Add possibilities to save more data.
    To reduce the amount of output data, it is now possible to save the mean
    and standard deviation of the avalanche position instead of every single
    electron position.

--- simulation/avalanche/avalanche.cpp
+++ simulation/avalanche/avalanche.cpp
@@ -110,12 +110,15 @@ int main(int argc, char * argv[]) {
 
    Int_t eventID;
    Int_t nele_drift; // number of electrons from drift
+   Int_t nele_gain;
    Int_t nele;  // number of electrons in avalanche
    Int_t nelep; // number of electron end points
    Double_t gain; // gain per electron
    vector<Int_t> status;
    vector<Double_t> x0, y0, z0, e0, t0;
    vector<Double_t> x1, y1, z1, e1, t1;
+   Double_t x1_mean, y1_mean;
+   Double_t x1_std, y1_std;
    vector<Double_t> signal_t;
    vector<Double_t> signal_amplitude;
 
@@ -124,12 +127,15 @@ int main(int argc, char * argv[]) {
    TTree* outputTree = new TTree("avalancheTree", "Avalanches");
    outputTree->Branch("eventID", &eventID);
    outputTree->Branch("nele_drift", &nele_drift, "nele_drift/I");
+   outputTree->Branch("nele_gain", &nele_gain, "nele_gain/I");
    outputTree->Branch("nele", &nele, "nele/I");
    outputTree->Branch("nelep", &nelep, "nelep/I");
    outputTree->Branch("gain", &gain, "gain/D");
    outputTree->Branch("status", &status);
    outputTree->Branch("x0", &x0); outputTree->Branch("y0", &y0); outputTree->Branch("z0", &z0); outputTree->Branch("e0", &e0); outputTree->Branch("t0", &t0);
    outputTree->Branch("x1", &x1); outputTree->Branch("y1", &y1); outputTree->Branch("z1", &z1); outputTree->Branch("e1", &e1); outputTree->Branch("t1", &t1);
+   outputTree->Branch("x1_mean", &x1_mean, "x1_mean/D"); outputTree->Branch("y1_mean", &y1_mean, "y1_mean/D");
+   outputTree->Branch("x1_std", &x1_std, "x1_std/D"); outputTree->Branch("y1_std", &y1_std, "y1_std/D");
    outputTree->Branch("signal_t", &signal_t);
    outputTree->Branch("signal_amplitude", &signal_amplitude);
    /*[[[cog
@@ -276,7 +282,12 @@ int main(int argc, char * argv[]) {
       numberOfElectrons = inNele;
       nele_drift = numberOfElectrons;
       nele = 0;
+      nele_gain = 0;
       nelep = 0;
+      x1_mean = 0;
+      y1_mean = 0;
+      x1_std = 0;
+      y1_std = 0;
 
       for (int e = 0; e < numberOfElectrons; e++) {
          // Set the initial position [cm], direction, starting time [ns] and initial energy [eV]
@@ -326,13 +337,14 @@ int main(int argc, char * argv[]) {
          for (int j = 0; j < np; j++) {
             avalanchemicroscopic->GetElectronEndpoint(j, xi, yi, zi, ti, ei, xf, yf, zf, tf, ef, stat);
 
-            /*[[[cog
-            from MMconfig import *
-            if conf['amplification'].getboolean('save_electron_coordinates'):
-               cog.outl('x0.push_back(xi); y0.push_back(yi); z0.push_back(zi);')
-               cog.outl('x1.push_back(xf); y1.push_back(yf); z1.push_back(zf);')
-            ]]]*/
-            //[[[end]]]
+            if (zf <= -25e-4) {
+               nele_gain++;
+               x1_mean += xf;
+               y1_mean += yf;
+            }
+
+            x0.push_back(xi); y0.push_back(yi); z0.push_back(zi);
+            x1.push_back(xf); y1.push_back(yf); z1.push_back(zf);
             e0.push_back(ei);
             e1.push_back(ef);
 
@@ -377,7 +389,27 @@ int main(int argc, char * argv[]) {
       ]]]*/
       //[[[end]]]
 
-      gain = (double)nele / (double)nele_drift;
+      gain = (double)nele_gain / (double)nele_drift;
+      x1_mean /= (double)nele_gain;
+      y1_mean /= (double)nele_gain;
+      for (unsigned int i = 0; i < x1.size(); i++) {
+         if (z1.at(i) <= 25e-4) {
+            x1_std += (x1.at(i) - x1_mean) * (x1.at(i) - x1_mean);
+            y1_std += (y1.at(i) - y1_mean) * (y1.at(i) - y1_mean);
+         }
+      }
+      x1_std /= (double)nele_gain;
+      y1_std /= (double)nele_gain;
+      /*[[[cog
+      from MMconfig import *
+      if not conf['amplification'].getboolean('save_electron_coordinates'):
+         cog.outl('x0.clear(); y0.clear(); z0.clear();')
+         cog.outl('x1.clear(); y1.clear(); z1.clear();')
+         cog.outl('e0.clear(); t0.clear();')
+         cog.outl('e1.clear(); t1.clear();')
+         cog.outl('status.clear();')
+      ]]]*/
+      //[[[end]]]
       outputTree->Fill();
       x0.clear(); y0.clear(); z0.clear(); e0.clear(); t0.clear();
       x1.clear(); y1.clear(); z1.clear(); e1.clear(); t1.clear();
@@ -396,7 +428,7 @@ int main(int argc, char * argv[]) {
 
    /*[[[cog
    from MMconfig import *
-   if conf.getboolean('amplification', 'save_hists', fallback=False):
+   if False and conf.getboolean('amplification', 'save_hists', fallback=False):
       for var_name, num_bins in eval(conf.get('amplification', 'save_variables', fallback='{}')).items():
          cog.outl('''
          outputTree->Draw("{name}>>htemp({num_bins})");
@@ -405,7 +437,7 @@ int main(int argc, char * argv[]) {
          {name}_hist->Write();
          delete {name}_hist;
          '''.format(name=var_name, num_bins=num_bins))
-   if not conf.getboolean('amplification', 'save_tree', fallback=True):
+   if False and not conf.getboolean('amplification', 'save_tree', fallback=True):
       cog.outl('delete outputTree;')
    ]]]*/
    //[[[end]]]
