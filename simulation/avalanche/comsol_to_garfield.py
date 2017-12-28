9c344a7100fc03d6e74e66f2ba90ad8bf2de5fee
Friedemann Neuhaus <fneuhaus@students.uni-mainz.de>
2017-12-28 13:23:59 +0100

    Write cache file when convertings meshes.

--- simulation/avalanche/comsol_to_garfield.py
+++ simulation/avalanche/comsol_to_garfield.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
+import os.path
 import numpy as np
 
 
@@ -39,9 +40,15 @@ def main():
    potential_in_wire = float(sys.argv[3]) if len(sys.argv) >= 4 else 0.
    scale_factor = float(sys.argv[4]) if len(sys.argv) >= 5 else 1.
    print('Reading: \'{}\''.format(file_name))
+
+   file_name_clean_base = os.path.splitext(os.path.basename(file_name_clean))[0]
+   file_name_cache = os.path.join(os.path.dirname(file_name_clean), f'.{file_name_clean_base}.cache')
+
    input_data = np.genfromtxt(file_name, comments='%', dtype='float')
-   output_data = np.zeros((input_data.shape[0],input_data.shape[1]+1))
+   output_data = np.zeros((input_data.shape[0], input_data.shape[1] + 1))
    output_data[:,:-1] = input_data
+   x_min, y_min, z_min = 10000, 10000, 10000
+   x_max, y_max, z_max = 0, 0, 0
 
    for i in range(len(output_data)):
       # COMSOL gives NaN for inside wire
@@ -49,15 +56,33 @@ def main():
       if np.isnan(output_data[i][6]):
          output_data[i][3:7] = np.array([0., 0., 0., potential_in_wire])
          output_data[i][-1] = 1
-      #  output_data[i][0:3] = 1000 * output_data[i][0:3]
-      #  output_data[i][3:6] = output_data[i][3:6] / 10
+      if output_data[i][0] < x_min:
+         x_min = output_data[i][0]
+      if output_data[i][0] > x_max:
+         x_max = output_data[i][0]
+      if output_data[i][1] < y_min:
+         y_min = output_data[i][1]
+      if output_data[i][1] > y_max:
+         y_max = output_data[i][1]
+      if output_data[i][2] < z_min:
+         z_min = output_data[i][2]
+      if output_data[i][2] > z_max:
+         z_max = output_data[i][2]
 
    #  print('Plotting to \'field.png\'...')
    #  plot_field(output_data)
 
    print('Writing: \'{}\''.format(file_name_clean))
    np.savetxt(file_name_clean, output_data, fmt=['%.7g']*7+['%d'])
-   print('x: [{}e-4,{}e-4]\ny: [{}e-4,{}e-4]\nz: [{}e-4,{}e-4]'.format(output_data[:,0].min(), output_data[:,0].max(), output_data[:,1].min(), output_data[:,1].max(), output_data[:,2].min(), output_data[:,2].max()))
+
+   x_len, y_len, z_len = len(set(output_data[:,0])), len(set(output_data[:,1])), len(set(output_data[:,2]))
+   result = 'fm->SetMesh({}, {}, {}, {}e-4, {}e-4, {}e-4, {}e-4, {}e-4, {}e-4);'.format(
+      x_len, y_len, z_len, x_min, x_max, y_min, y_max, z_min - 0.1, z_max + 0.1)
+   with open(file_name_cache, 'w') as f:
+      f.write(result)
+   print('Writing cache file: "{}"'.format(file_name_cache))
+
+   print('x: [{}e-4, {}e-4]\ny: [{}e-4, {}e-4]\nz: [{}e-4, {}e-4]'.format(x_min, x_max, y_min, y_max, z_min, z_max))
 
 
 if __name__=='__main__':
